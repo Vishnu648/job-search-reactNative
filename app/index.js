b21f9ce63a4be377bd265c7133ed2b1905fb5179
VishnuVS <vishnusenthil222@gmail.com>
2023-09-22 20:52:51 +0530

    imported constants and components to app/index.js

--- app/index.js
+++ app/index.js
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native'
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView, Text } from 'react-native';
+import { Stack, useRouter } from 'expo-router'
+
+import { COLORS, SIZES, icons, images } from '../constants'
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome
+} from "../components"
+
 
 const Home = () => {
   return (
-    <View style={{padding:20}}>
+    <SafeAreaView >
       <Text>Home</Text>
       <Text>lorem ipsum lorem ipsum lorem ipsum lorem ipsum </Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
